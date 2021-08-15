2e0b6d34ce6fc37001dde18e7d4185afe6194351
leticiaoliveira5 <olivaleticia@hotmail.com>
2021-08-15 16:21:09 -0300

    Controller e testes de Step

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,8 +1,11 @@
 class Step < ApplicationRecord
   belongs_to :milestone
-  belongs_to :goal
 
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 50 }
 
   enum status: { inactive: 0, in_progress: 1, completed: 2 }
+
+  def goal
+    Goal.find(milestone.goal_id)
+  end
 end
--- spec/models/step_spec.rb
+++ spec/models/step_spec.rb
@@ -3,5 +3,16 @@ require 'rails_helper'
 RSpec.describe Step, type: :model do
   context 'validation' do
     it { should validate_presence_of(:title) }
+    it { should validate_length_of(:title) }
+  end
+  
+  context '#goal' do
+    it 'returns goal' do
+      goal = create(:goal)
+      milestone = create(:milestone, goal: goal)
+      step = create(:step, milestone: milestone)
+
+      expect(step.goal).to eq goal
+    end
   end
 end
