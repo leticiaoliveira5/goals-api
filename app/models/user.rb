8bddf7b46cc2fdf34deea43f152c2020e4c88797
leticiaoliveira5 <olivaleticia@hotmail.com>
2021-08-29 15:27:54 -0300

    seta json como formato devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable,
          :jwt_authenticatable,
          :registerable,
+         :validatable,
          jwt_revocation_strategy: JwtDenylist
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users,
-      controllers: {
-      sessions: 'users/sessions',
-      registrations: 'users/registrations'
-      }
+             controllers: { sessions: 'users/sessions', registrations: 'users/registrations' },
+             defaults: { format: :json }
+             
   get '/member-data', to: 'members#show'
+
   namespace 'api' do
     namespace 'v1' do
       resources :goals, only: %i[index show create destroy update edit]
       resources :milestones, only: %i[show create destroy update edit]
       resources :steps, only: %i[show create destroy update edit]
-    end  
+    end
   end
 end
