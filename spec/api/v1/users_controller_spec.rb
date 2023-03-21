743d0b54561678ded5360d2891d390a8379415f9
leticiaoliveira5 <olivaleticia@hotmail.com>
2023-03-21 10:25:42 -0300

    rubocop

--- spec/api/v1/users_controller_spec.rb
+++ spec/api/v1/users_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'UsersController', type: :request do
+RSpec.describe Api::V1::UsersController, type: :request do
   let(:user) { create(:user) }
   let(:user_keys) { { user: { email: user.email, password: user.password } }.as_json }
   let(:token) { { Authorization: response.header['Authorization'] }.as_json }
@@ -8,7 +8,7 @@ describe 'UsersController', type: :request do
   context 'show' do
     let(:expected_response) { { user: user }.to_json }
 
-    it 'should return user as json' do
+    it 'returns user as json' do
       post user_session_path(user_keys)
       get api_v1_user_path(user.id), headers: token
 
