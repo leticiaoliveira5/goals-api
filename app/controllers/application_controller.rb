1804ea00263e0547605d31b006c2081a7689522c
leticiaoliveira5 <olivaleticia@hotmail.com>
2023-03-21 10:18:13 -0300

    rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::API
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password) }
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :email, :password, :current_password) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:username, :email, :password, :current_password)
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context 'validation' do
-    it { should validate_presence_of(:email) }
-    it { should validate_presence_of(:password) }
-    it { should validate_presence_of(:username) }
-    it { should validate_length_of(:username) }
-    it { should validate_uniqueness_of(:username) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:password) }
+    it { is_expected.to validate_presence_of(:username) }
+    it { is_expected.to validate_length_of(:username) }
+    it { is_expected.to validate_uniqueness_of(:username) }
   end
 end
